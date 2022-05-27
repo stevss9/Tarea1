30b0cf46a241d281a099df6cd6e46a01ca6f2dd4
Steveens97 <sinchiguanomoreira@gmail.com>
2022-05-26 20:59:27 -0500

    creamos un controlador con componentes

---
+++ aplicacion.py
@@ -0,0 +1,18 @@
+#Importamos las librerias
+from flask import Flask, redirect, render_template, request, url_for, flash
+
+#Objeto para inicilizar la aplicacion
+app = Flask(__name__, template_folder='templates')
+
+#Clave  de la app
+app.secret_key = '090700'
+
+#Controlador de la ruta inicial
+@app.route('/')
+def principal():
+    return render_template('principal.html')
+
+#Main de la app
+if __name__ == '__main__':
+    app.run(debug=True)
+    
